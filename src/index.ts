f8d8087e3d5fab788c3e72dd380296f4ea20af93
Ryan Luong <lumintriet@gmail.com>
2026-09-18 19:29:31 +0700

    feat: define branded type.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+declare const __brand: unique symbol;
+
+type Brand<B> = { [__brand]: B };
+
+export type Branded<T, B> = Brand<B> & T;
+
 export type OmitKnownKeys<T, K extends keyof T> = Omit<T, K>;
 
 export type ValueOf<T extends object> = T[keyof T];
