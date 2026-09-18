5828881203f2c9fa8989557f74f96ddffa24c21b
Ryan Luong <lumintriet@gmail.com>
2026-09-18 19:30:02 +0700

    style

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 declare const __brand: unique symbol;
-
 type Brand<B> = { [__brand]: B };
-
 export type Branded<T, B> = Brand<B> & T;
 
 export type OmitKnownKeys<T, K extends keyof T> = Omit<T, K>;
