faa51861384df55c2afb49eeeeb8162429a6e60b
Ryan Luong <lumintriet@gmail.com>
2026-09-22 10:22:34 +0700

    feat: define ExtractLiteral and ExcludeLiteral types.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ type Brand<B> = { [__brand]: B };
 export type Branded<T, B> = Brand<B> & T;
 
 export type OmitKnownKeys<T, K extends keyof T> = Omit<T, K>;
-
 export type ValueOf<T extends object> = T[keyof T];
+
+export type ExtractLiteral<T, U extends T> = Extract<T, U>;
+export type ExcludeLiteral<T, U extends T> = Exclude<T, U>;
